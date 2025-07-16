76c5b4661f1eeb6cc5b113723aaa1d76a81cbc01
David Orrego <orregod091@gmail.com>
2025-07-16 17:09:08 -0400

    {Update} 1. Add properties  -ICollection<Animal> Animals  -DateTime JoinedAt

--- Core/Models/AppUser.cs
+++ Core/Models/AppUser.cs
@@ -4,9 +4,11 @@ namespace Core.Models;
 
 public class AppUser : IdentityUser
 {
-    public AppUser(string email,string userName)
+    public AppUser(string email, string userName)
     {
         Email = email;
         UserName = userName;
     }
+    public ICollection<Animal> Animals { get; set; } = new List<Animal>();
+    public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
 }
