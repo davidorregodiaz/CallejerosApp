4f267ed0f9958456dcf9fb604ce77c62f65bccc0
David Orrego <orregod091@gmail.com>
2025-08-13 02:21:20 -0400

    {Modified} 1. Add static files from server and from client

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,5 @@
 using Api.Extensions;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,16 +10,34 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi("/openapi/docs.json");
 }
 
-app.UseHttpsRedirection();
+var clientWwwRoot = Path.Combine(builder.Environment.ContentRootPath, "..", "Client", "wwwroot");
+// app.UseHttpsRedirection();
+
+app.UseCors("AllowBlazorClient");
 
 app.UseCookiePolicy();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(clientWwwRoot),
+    RequestPath = ""
+});
+
+// //Servir imagenes del servidor
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.WebRootPath, "images/upload")),
+    RequestPath = "/wwwroot/images/upload"
+});
+
+app.UseBlazorFrameworkFiles();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
