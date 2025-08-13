e9c72f3c213c716c2479e0571f49a56f48934688
David Orrego <orregod091@gmail.com>
2025-08-13 02:20:40 -0400

    {Modified} 1. Add HttpClientFactory 2. Add MessageHandler JwtTokenMessageHandler 2. Inject the auth service as singleton

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-    {
-        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-    });
+var appUri = builder.HostEnvironment.BaseAddress;
+
+builder.Services.AddSingleton<AuthService>();
+builder.Services.AddSingleton<AuthenticationStateProvider>(provider => provider.GetRequiredService<AuthService>());
+
+builder.Services.AddScoped(provider => new JwtTokenMessageHandler(new Uri(appUri), provider.GetRequiredService<AuthService>()));
+
+builder.Services.AddHttpClient("Api", client => client.BaseAddress = new Uri(appUri))
+    .AddHttpMessageHandler<JwtTokenMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Api"));
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AnimalService>();
+
 
 await builder.Build().RunAsync();
 
